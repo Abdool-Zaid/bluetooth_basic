a6251b751ca7e97662e74cbcc81248986eb0862c
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-04-24 14:19:52 +0200

    'update'

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
-console.log('working')
-// Request permission to use the Web Bluetooth API
 
-let btn= document.querySelector("#btn")
-btn.addEventListener('click',()=>{
-
-    
-    
-    navigator.bluetooth.requestDevice({ acceptAllDevices:true
-        // filters: [{ services: ['battery_service'] }]
+let btn = document.querySelector("#btn")
+btn.addEventListener('click', () => {
+  navigator.bluetooth.requestDevice({
+      acceptAllDevices: true
     })
-  .then(device => {
-        // Connect to the selected device
-        return device.gatt.connect();
-      })
-      .then(server => {
-            // Get the Battery Service from the connected device
-            return server.getPrimaryService('battery_service');
-  })
-  .then(service => {
-        // Get the Battery Level characteristic from the Battery Service
-        return service.getCharacteristic('battery_level');
-      })
-      .then(characteristic => {
-            // Read the Battery Level value from the characteristic
-            return characteristic.readValue();
-          })
-  .then(value => {
-    // Convert the value to a percentage
-    const batteryLevel = value.getUint8(0);
-    console.log(`Battery level is ${batteryLevel}%`);
-  })
-  .catch(error => {
-        console.error(error);
-  });
-  
-})
+    .then(device => {
+      // Connect to the selected device
+      return device.gatt.connect();
+    })
+    .then(server => {
+      // Get the list of services
+      return server.getPrimaryServices();
+    })
+    .then(services => {
+      // Display the UUIDs of each service
+      services.forEach(service => {
+        console.log('Service UUID:', service.uuid);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
 
 // paired devices
 
